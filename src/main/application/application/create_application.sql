2ca45b096fc9552836d990f57149c2954844a5dd
Jose Javier Gálvez Gómez <jpromocion@gmail.com>
2023-09-11 14:01:31 +0200

    Actualizada version

--- src/main/application/application/create_application.sql
+++ src/main/application/application/create_application.sql
@@ -38,7 +38,7 @@ wwv_imp_workspace.create_flow(
 ,p_public_user=>'APEX_PUBLIC_USER'
 ,p_proxy_server=>nvl(wwv_flow_application_install.get_proxy,'')
 ,p_no_proxy_domains=>nvl(wwv_flow_application_install.get_no_proxy_domains,'')
-,p_flow_version=>'v1.4'
+,p_flow_version=>'v1.5'
 ,p_flow_status=>'AVAILABLE_W_EDIT_LINK'
 ,p_flow_unavailable_text=>unistr('La aplicaci\00F3n no est\00E1 disponible en este momento')
 ,p_exact_substitutions_only=>'Y'
@@ -53,7 +53,7 @@ wwv_imp_workspace.create_flow(
 ,p_substitution_string_01=>'APP_NAME'
 ,p_substitution_value_01=>'Recetario'
 ,p_last_updated_by=>'JORTRI'
-,p_last_upd_yyyymmddhh24miss=>'20230911111437'
+,p_last_upd_yyyymmddhh24miss=>'20230911115834'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>31
 ,p_print_server_type=>'NATIVE'
